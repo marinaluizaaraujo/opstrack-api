1d8643affa17f83c0636c35c4d267ab17991d010
marinaluizaaraujo <mluizadearaujopaiva@gmail.com>
2026-08-17 10:14:34 -0300

    feat: add rota de tickets com lista de tickets simulada

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ app = Flask (__name__)
 def status():
     return {'Servico': 'OpsTrackAPI', 'status': 'online'}
 
+@app.route('/tickets')
+def tickets():
+    return {
+        'tickets': [
+            {'id': 1, 'titulo': 'Erro ao logar no sistema', 'status': 'aberto'},
+            {'id': 2, 'titulo': 'Lentidão no relatório mensal', 'status': 'em andamento'},
+            {'id': 3, 'titulo': 'Solicitação de novo acesso', 'status': 'fechado'}
+        ]
+    }
+
 if __name__ == '__main__':
     app.run(debug=True)
+
